ad52db0a6eaa27b22a92bd1bd98d092c193e5eb3
benji694 <bbouillard@edenschool.fr>
2022-04-20 11:11:52 +0200

    fetch api and add quote to list

--- js/app.js
+++ js/app.js
@@ -1,16 +1,32 @@
 const btnAdd = document.querySelector("#btnAdd");
 const list = document.querySelector("#list");
-let data = ["test1", "test2"];
+let quotes = [];
 
 const render = () => {
   list.innerHTML = "";
-  for (let i = 0; i < data.length; i++) {
+  for (let i = 0; i < quotes.length; i++) {
     let element = document.createElement("li");
-    element.textContent = data[i];
+    element.textContent = quotes[i];
     list.appendChild(element);
   }
 };
 
 btnAdd.addEventListener("click", () => {
+  render();
+  fetch("https://api.kanye.rest/")
+    .then((response) => response.json())
+    .then((data) => {
+      if (!quotes.includes(data.quote)) {
+        quotes.push(data.quote);
+      } else {
+        const header = document.querySelector("header");
+        const p = document.createElement("p");
+        p.style.color = "red";
+        p.textContent =
+          "Désoler, cette citation est déjà afficher ci-dessous...";
+        header.appendChild(p);
+      }
+    })
+    .catch((error) => console.log(error));
   render();
 });

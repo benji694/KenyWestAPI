fa6d204a383785e11ee23d4191afcf71a89b5786
benji694 <bbouillard@edenschool.fr>
2022-04-20 11:39:49 +0200

    create up and down button

--- js/app.js
+++ js/app.js
@@ -1,25 +1,20 @@
 const btnAdd = document.querySelector("#btnAdd");
 const list = document.querySelector("#list");
+const header = document.querySelector("header");
 let quotes = [];
 
-const render = () => {
-  list.innerHTML = "";
-  for (let i = 0; i < quotes.length; i++) {
-    let element = document.createElement("li");
-    element.textContent = quotes[i];
-    list.appendChild(element);
-  }
-};
-
 btnAdd.addEventListener("click", () => {
-  render();
+  if (header.children.length > 3) {
+    const p = header.querySelector("p");
+    header.removeChild(p);
+  }
   fetch("https://api.kanye.rest/")
     .then((response) => response.json())
     .then((data) => {
       if (!quotes.includes(data.quote)) {
         quotes.push(data.quote);
+        console.log(quotes);
       } else {
-        const header = document.querySelector("header");
         const p = document.createElement("p");
         p.style.color = "red";
         p.textContent =
@@ -30,3 +25,19 @@ btnAdd.addEventListener("click", () => {
     .catch((error) => console.log(error));
   render();
 });
+
+const render = () => {
+  list.innerHTML = "";
+  for (let i = 0; i < quotes.length; i++) {
+    let element = document.createElement("li");
+    element.innerHTML = `
+        <p>${quotes[i]}</p>
+        <div class='buttonGroup'>
+            <button class='btnUp'>UP</button>
+            <button class='btnDown'>DOWN</button>
+        </div>
+    `;
+
+    list.appendChild(element);
+  }
+};

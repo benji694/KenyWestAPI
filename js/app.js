d61ba5d1236b5d7580d183b353b1e1f4708a7484
benji694 <bbouillard@edenschool.fr>
2022-04-20 10:56:27 +0200

    add event listener on the button and render function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log("hello");
+const btnAdd = document.querySelector("#btnAdd");
+const list = document.querySelector("#list");
+let data = ["test1", "test2"];
+
+const render = () => {
+  list.innerHTML = "";
+  for (let i = 0; i < data.length; i++) {
+    let element = document.createElement("li");
+    element.textContent = data[i];
+    list.appendChild(element);
+  }
+};
+
+btnAdd.addEventListener("click", () => {
+  render();
+});

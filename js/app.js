410f4dc9f3a8b4e977add9fe73c82be7b34d9415
benji694 <bbouillard@edenschool.fr>
2022-04-22 14:31:09 +0200

    add css and take my break

--- js/app.js
+++ js/app.js
@@ -1,15 +1,17 @@
 $("#btnAdd").click(() => {
   $.get("https://api.kanye.rest/", (data) => {
     let p = $("<p></p>").text(data.quote);
-    let btnUp = $("<button></button>").text("UP");
+    let btnUp = $("<button><i class='fa-solid fa-arrow-up'></i></button>");
     btnUp.click(() => {
       btnUp.parent().parent().prev().before(btnUp.parent().parent());
     });
-    let btnDown = $("<button></button>").text("DOWN");
+    let btnDown = $("<button><i class='fa-solid fa-arrow-down'></i></button>");
     btnDown.click(() => {
       btnDown.parent().parent().next().after(btnDown.parent().parent());
     });
-    let buttonGroup = $("<div></div>").append(btnUp, btnDown);
+    let buttonGroup = $("<div></div>")
+      .addClass("buttonGroup")
+      .append(btnUp, btnDown);
     let element = $("<li></li>").append(p, buttonGroup);
     $("#list").append(element);
   });
